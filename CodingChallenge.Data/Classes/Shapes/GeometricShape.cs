2f968d2bdfd9cfa6ec11d80558723f5d64067b28
MaarLop <s.mariel.lopez1995@gmail.com>
2023-10-17 22:38:51 -0300

    refactor: Se cambia nombre de clase y se deja codigo necesario en la clase abstracta

---
+++ CodingChallenge.Data/Classes/Shapes/GeometricShape.cs
@@ -0,0 +1,20 @@
+﻿using CodingChallenge.Data.Recursos;
+using CodingChallenge.Guards;
+
+namespace CodingChallenge.Data.Classes.Shapes
+{
+    public abstract class GeometricShape
+    {
+        protected decimal Side { get; }
+
+        protected GeometricShape(decimal side)
+        {
+            Guard.Against.NegativeOrZero(side, ResX.Side_of_a_shape_must_be_positive);
+            Side = side;
+        }
+
+        public abstract decimal CalculateArea();
+
+        public abstract decimal CalculatePerimeter();
+    }
+}
