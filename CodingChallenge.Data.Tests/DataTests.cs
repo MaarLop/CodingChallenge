34bb9cdabe44ccb660c47ffd5070a7d5f577f085
MaarLop <s.mariel.lopez1995@gmail.com>
2023-10-17 22:42:34 -0300

    refactor: Se cambia lo necesario para que los test respete lo nuevo + Se cambia de nombre a los metodos para evitar spanglish

--- CodingChallenge.Data.Tests/DataTests.cs
+++ CodingChallenge.Data.Tests/DataTests.cs
@@ -1,92 +1,125 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using CodingChallenge.Data.Classes;
+using CodingChallenge.Data.Classes.Shapes;
+using CodingChallenge.Data.Enums;
+using CodingChallenge.Data.Factories;
+using CodingChallenge.Data.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
 
 namespace CodingChallenge.Data.Tests
 {
     [TestFixture]
     public class DataTests
     {
-        [TestCase]
-        public void TestResumenListaVacia()
+        private readonly Report _report = new Report();
+
+        [TestInitialize]
+        public void Before()
         {
-            Assert.AreEqual("<h1>Lista vacía de formas!</h1>",
-                FormaGeometrica.Imprimir(new List<FormaGeometrica>(), 1));
         }
 
         [TestCase]
-        public void TestResumenListaVaciaFormasEnIngles()
+        public void TestEmptyListInSpanish()
         {
-            Assert.AreEqual("<h1>Empty list of shapes!</h1>",
-                FormaGeometrica.Imprimir(new List<FormaGeometrica>(), 2));
+            LanguageHelper.SetSpanish();
+            var expected = "<h1>Lista vacía de formas!</h1>";
+            Assert.AreEqual(expected,
+                _report.GenerateReport(new List<GeometricShape>()));
         }
 
         [TestCase]
-        public void TestResumenListaConUnCuadrado()
+        public void TestEmptyListInEnglish()
         {
-            var cuadrados = new List<FormaGeometrica> {new FormaGeometrica(FormaGeometrica.Cuadrado, 5)};
+            LanguageHelper.SetEnglish();
 
-            var resumen = FormaGeometrica.Imprimir(cuadrados, FormaGeometrica.Castellano);
-
-            Assert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 formas Perimetro 20 Area 25", resumen);
+            var expected = "<h1>Empty list of shapes!</h1>";
+            Assert.AreEqual(expected,
+                _report.GenerateReport(new List<GeometricShape>()));
         }
 
         [TestCase]
-        public void TestResumenListaConMasCuadrados()
+        public void TestReportWithASingleSquare()
         {
-            var cuadrados = new List<FormaGeometrica>
+            var squares = new List<GeometricShape> { ShapeFactory.CreateShape(Shape.Square, 5) };
+
+            LanguageHelper.SetSpanish();
+
+            var report = _report.GenerateReport(squares);
+
+            var expected = "<h1>Reporte de Formas</h1>1 Cuadrado | Área 25 | Perímetro 20 <br/>TOTAL:<br/>1 forma Perímetro 20 Área 25";
+            Assert.AreEqual(expected, report);
+        }
+
+        [TestCase]
+        public void TestReportWithASeveralSquares()
+        {
+            LanguageHelper.SetEnglish();
+
+            var squares = new List<GeometricShape>
             {
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 5),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 1),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 3)
+                ShapeFactory.CreateShape(Shape.Square, 5),
+                ShapeFactory.CreateShape(Shape.Square, 1),
+                ShapeFactory.CreateShape(Shape.Square, 3)
             };
 
-            var resumen = FormaGeometrica.Imprimir(cuadrados, FormaGeometrica.Ingles);
+            var report = _report.GenerateReport(squares);
 
-            Assert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35", resumen);
+            var expected = "<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35";
+            Assert.AreEqual(expected, report);
         }
 
         [TestCase]
-        public void TestResumenListaConMasTipos()
+        public void TestReportWithSeveralShapesInEnglish()
         {
-            var formas = new List<FormaGeometrica>
+            LanguageHelper.SetEnglish();
+            var shapes = new List<GeometricShape>
             {
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 5),
-                new FormaGeometrica(FormaGeometrica.Circulo, 3),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 2),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 9),
-                new FormaGeometrica(FormaGeometrica.Circulo, 2.75m),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4.2m)
+                ShapeFactory.CreateShape(Shape.Square, 5),
+                ShapeFactory.CreateShape(Shape.Circle, 3),
+                ShapeFactory.CreateShape(Shape.EquilateralTriangle, 4),
+                ShapeFactory.CreateShape(Shape.Square, 2),
+                ShapeFactory.CreateShape(Shape.EquilateralTriangle, 9),
+                ShapeFactory.CreateShape(Shape.Circle, 2.75m),
+                ShapeFactory.CreateShape(Shape.EquilateralTriangle, 4.2m)
             };
 
-            var resumen = FormaGeometrica.Imprimir(formas, FormaGeometrica.Ingles);
+            var report = _report.GenerateReport(shapes);
 
-            Assert.AreEqual(
-                "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13,01 | Perimeter 18,06 <br/>3 Triangles | Area 49,64 | Perimeter 51,6 <br/>TOTAL:<br/>7 shapes Perimeter 97,66 Area 91,65",
-                resumen);
+            var expected =
+                "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13.01 | Perimeter 18.06 <br/>3 Triangles | Area 49.64 | Perimeter 51.6 <br/>TOTAL:<br/>7 shapes Perimeter 97.66 Area 91.65";
+            Assert.AreEqual(expected, report);
         }
 
         [TestCase]
-        public void TestResumenListaConMasTiposEnCastellano()
+        public void TestReportWithSeveralShapesInSpanish()
         {
-            var formas = new List<FormaGeometrica>
+            LanguageHelper.SetSpanish();
+
+            var shapes = new List<GeometricShape>
             {
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 5),
-                new FormaGeometrica(FormaGeometrica.Circulo, 3),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 2),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 9),
-                new FormaGeometrica(FormaGeometrica.Circulo, 2.75m),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4.2m)
+                ShapeFactory.CreateShape(Shape.Square, 5),
+                ShapeFactory.CreateShape(Shape.Circle, 3),
+                ShapeFactory.CreateShape(Shape.EquilateralTriangle, 4),
+                ShapeFactory.CreateShape(Shape.Square, 2),
+                ShapeFactory.CreateShape(Shape.EquilateralTriangle, 9),
+                ShapeFactory.CreateShape(Shape.Circle, 2.75m),
+                ShapeFactory.CreateShape(Shape.EquilateralTriangle, 4.2m)
             };
 
-            var resumen = FormaGeometrica.Imprimir(formas, FormaGeometrica.Castellano);
+            var report = _report.GenerateReport(shapes);
 
-            Assert.AreEqual(
-                "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13,01 | Perimetro 18,06 <br/>3 Triángulos | Area 49,64 | Perimetro 51,6 <br/>TOTAL:<br/>7 formas Perimetro 97,66 Area 91,65",
-                resumen);
+            var expected =
+                "<h1>Reporte de Formas</h1>2 Cuadrados | Área 29 | Perímetro 28 <br/>2 Círculos | Área 13,01 | Perímetro 18,06 <br/>3 Triángulos | Área 49,64 | Perímetro 51,6 <br/>TOTAL:<br/>7 formas Perímetro 97,66 Área 91,65";
+            Assert.AreEqual(expected, report);
         }
     }
+
+    //TODO:
+    //Ver que pasan con el trapecio y el rectangulo
+    //Refactorizar la clase de report (quizas un poco mas)
+    //Volver a dejar los csporj como estaban
+    //Probar los validadores
+    //cambiar nombre a las carpetas
 }
