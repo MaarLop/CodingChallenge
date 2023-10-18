326a9eb33a51dd755947fbf401ce9df7fc6320d1
MaarLop <s.mariel.lopez1995@gmail.com>
2023-10-17 22:41:13 -0300

    feate: Se agrega ResX para manejo de idiomas

---
+++ CodingChallenge.Data/Helpers/LanguageHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Threading;
+
+namespace CodingChallenge.Data.Helpers
+{
+    public static class LanguageHelper
+    {
+        //ver que se puede mejorar para no repetir lineas entre los idiomas
+        public static void SetSpanish()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("es-AR");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("es-AR");
+        }
+
+        public static void SetEnglish()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+        }
+    }
+}
